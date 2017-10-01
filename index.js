5487e34218ac37d2a678671362e0c06517f3d025
Mohammad Elbanna <mammelbanna@gmail.com>
2017-10-01 15:15:11 +0200

    - update server code to add a list of operations that is updated as users do operations. - added /initialOp route to intially retrieve the list of operations for new users

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
+var operations = [];
+
 app.use(express.static("client/build"));
+
+app.get("/initialOp", function(req, res) {
+  console.log("inside initialOp");
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.send(JSON.stringify(operations));
+});
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/client/build/index.html");
@@ -13,10 +22,14 @@ io.on("connection", function(socket) {
   console.log("a user connected");
 
   socket.on("operation", function(msg) {
+    if (operations.length === 10) {
+      operations.shift();
+    }
+    operations.push(msg);
     socket.broadcast.emit("operation", msg);
   });
 });
 
 http.listen(4000, function() {
-  console.log("listening on *:3000");
+  console.log("listening on *:4000");
 });
